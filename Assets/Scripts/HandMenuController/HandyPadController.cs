23be383c509665c34bdf68478512e39ce7879939
Antony Nasce <nasce@mac.com>
2025-04-07 15:52:07 +0100

    Adding events and start state. Arrow indicators possibly useful.

--- Assets/Scripts/HandMenuController/HandyPadController.cs
+++ Assets/Scripts/HandMenuController/HandyPadController.cs
@@ -2,12 +2,13 @@ using UnityEngine;
 using Cacophony;
 using UnityEngine.UI;
 using DG.Tweening;
+using UnityEngine.Events;
 
 public class HandyPadController : MonoBehaviour
 {
     /// <summary>
-    ///  Double Tap Summons and dismisses the control.
-    ///  Pinch+Move will trigger the four directional axis inputs.
+    ///  Grab Release summons and dismisses the menu.
+    ///  Pinch + Move will trigger the four directional axis inputs.
     /// </summary>
     ///
     public GestureConsumerUnityEvents showHideGesture;
@@ -17,17 +18,44 @@ public class HandyPadController : MonoBehaviour
     public GestureConsumerUnityEvents moveDownGesture;
     public GestureConsumerUnityEvents confirmGesture;
     public Image posIndicator;
-    public Vector2 moveDistance = new(100, 100);
+    public Vector2 moveDistance = new(108, 108);
 
-    public CanvasGroup mainCanvas;
-    private bool isShown = false;
+    public CanvasGroup mainIndicator;
+    public CanvasGroup arrowIndicators;
+    public bool hideOnStart;
+    public bool isShown = false;
     private bool isAnimating = false;
 
     [Header("Audio")]
+    public bool interfaceAudio = true;
     public AudioSource audioSource;
     public AudioClip showMenuAudio;
     public AudioClip hideMenuAudio;
     public AudioClip moveOptionAudio;
+
+    [Header("Events")]
+    public UnityEvent OnLeftSelected;
+    public UnityEvent OnRightSelected;
+    public UnityEvent OnUpSelected;
+    public UnityEvent OnDownSelected;
+    public UnityEvent OnMenuShown;
+    public UnityEvent OnMenuHidden;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        showHideGesture.OnGestureEnd.AddListener(HandleShowHide);
+        moveLeftGesture.OnGestureEnd.AddListener(HandleLeft);
+        moveRightGesture.OnGestureEnd.AddListener(HandleRight);
+        moveUpGesture.OnGestureEnd.AddListener(HandleUp);
+        moveDownGesture.OnGestureEnd.AddListener(HandleDown);
+        confirmGesture.OnGestureEnd.AddListener(HandleConfirm);
+        if (hideOnStart)
+        {
+            isShown = false;
+            HideMenu(false);
+        }
+    }
 
     private void PlayShowAudio()
     {
@@ -44,38 +72,42 @@ public class HandyPadController : MonoBehaviour
         audioSource.PlayOneShot(moveOptionAudio);
     }
 
-    public void ShowMenu()
+    public void ShowMenu(bool playAudio = true)
     {
         isAnimating = true;
-        mainCanvas.transform.DOScale(1, 0.3f);
-        mainCanvas.DOFade(1, 0.3f).OnComplete(() =>
+        mainIndicator.transform.DOScale(1, 0.3f);
+        mainIndicator.DOFade(1, 0.3f).OnComplete(() =>
         {
             isAnimating = false;
-            isShown = !isShown;
-            // If NOT shown, reset the position...
-            if (!isShown)
-            {
-                ResetMenuPosition();
-            }
+            isShown = true;
+            OnMenuShown.Invoke();
         });
-        PlayShowAudio();
+        if (interfaceAudio && playAudio)
+        {
+            PlayShowAudio();
+        }
     }
 
-    public void HideMenu()
+    public void HideMenu(bool playAudio = true)
     {
         isAnimating = true;
-        mainCanvas.transform.DOScale(0, 0.3f);
-        mainCanvas.DOFade(0, 0.3f).OnComplete(() =>
+        mainIndicator.transform.DOScale(0, 0.3f);
+        mainIndicator.DOFade(0, 0.3f).OnComplete(() =>
         {
             isAnimating = false;
-            isShown = !isShown;
+            isShown = false;
+            Debug.Log("HideMenu: isShown:" + isShown);
             // If NOT shown, reset the position...
             if (!isShown)
             {
                 ResetMenuPosition();
             }
+            OnMenuHidden.Invoke();
         });
-        PlayHideAudio();
+        if (interfaceAudio && playAudio)
+        {
+            PlayHideAudio();
+        }
     }
 
 
@@ -84,20 +116,14 @@ public class HandyPadController : MonoBehaviour
         // Returns true if menu is active and not transitioning
         return (isShown && !isAnimating);
     }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        showHideGesture.OnGestureEnd.AddListener(HandleShowHide);
-        moveLeftGesture.OnGestureEnd.AddListener(HandleLeft);
-        moveRightGesture.OnGestureEnd.AddListener(HandleRight);
-        moveUpGesture.OnGestureEnd.AddListener(HandleUp);
-        moveDownGesture.OnGestureEnd.AddListener(HandleDown);
-        confirmGesture.OnGestureEnd.AddListener(HandleConfirm);
-    }
 
     private void HandleShowHide()
     {
-        if (isShown)
+        // Toggle the isShown State and handle accordingly...
+        
+        isShown = !isShown;
+        Debug.Log("HandleShowHide, isShownState should be: " + isShown);
+        if (!isShown)
         {
             HideMenu();
         }
@@ -126,6 +152,7 @@ public class HandyPadController : MonoBehaviour
             posIndicator.rectTransform.DOAnchorPos(new Vector2(-moveDistance.x, 0f), 0.3f).OnComplete(() =>
             {
                 isAnimating = false;
+                OnLeftSelected.Invoke();
             });
         }
     }
@@ -138,6 +165,7 @@ public class HandyPadController : MonoBehaviour
             posIndicator.rectTransform.DOAnchorPos(new Vector2(moveDistance.x, 0f), 0.3f).OnComplete(() =>
             {
                 isAnimating = false;
+                OnRightSelected.Invoke();
             });
         }
     }
@@ -151,6 +179,7 @@ public class HandyPadController : MonoBehaviour
             posIndicator.rectTransform.DOAnchorPos(new Vector2(0f, moveDistance.y), 0.5f).OnComplete(() =>
             {
                 isAnimating = false;
+                OnUpSelected.Invoke();
             });
         }
     }
@@ -164,6 +193,7 @@ public class HandyPadController : MonoBehaviour
             posIndicator.rectTransform.DOAnchorPos(new Vector2(0f, -moveDistance.y), 0.5f).OnComplete(() =>
             {
                 isAnimating = false;
+                OnDownSelected.Invoke();
             });
         }
     }
